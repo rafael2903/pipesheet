6753bed8d3f27e6d121195ffc001679213cee6dc
Pedro Henrique dos Santos <pedro.santos@cjr.org.br>
2021-08-01 23:08:32 -0300

    ✨ Adds sass and convert the initial page to it

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,56 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import {
+  container,
+  main,
+  title,
+  description,
+  grid,
+  card,
+  footer,
+  logo,
+} from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={container}>
       <Head>
         <title>PipeSheet</title>
-        <meta name="description" content="PipeSheet - Integre seus pipes e planilhas" />
+        <meta
+          name="description"
+          content="PipeSheet - Integre seus pipes e planilhas"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          <Image src="/logo.svg" alt="PipeSheet Logo" width={300} height={200} />  
+      <main className={main}>
+        <h1 className={title}>
+          <Image
+            src="/logo.svg"
+            alt="PipeSheet Logo"
+            width={300}
+            height={200}
+          />
         </h1>
 
-        <p className={styles.description}>
+        <p className={description}>
           Tenha os dados dos seus pipes em suas planilhas
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+        <div className={grid}>
+          <a href="https://nextjs.org/docs" className={card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="https://nextjs.org/learn" className={card}>
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+            className={card}
           >
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
@@ -41,7 +58,7 @@ export default function Home() {
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            className={card}
           >
             <h2>Deploy &rarr;</h2>
             <p>
@@ -51,14 +68,14 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className={footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <span className={styles.logo}>
+          <span className={logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
