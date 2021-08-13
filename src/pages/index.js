8c927b06914e801b0e8fcf183c594be0652db44f
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2021-08-13 16:56:44 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,24 @@ export default function Home() {
   return (
     <div className="flex container mx-auto h-screen w-screen justify-center items-center overflow-hidden">
       <Head>
+        <!-- Primary Meta Tags -->
         <title>PipeSheet</title>
-        <meta
-          name="description"
-          content="PipeSheet - Integre seus pipes e planilhas"
-        />
+        <meta name="title" content="PipeSheet">
+        <meta name="description" content="Integre seus pipes e planilhas">
+
+        <!-- Open Graph / Facebook -->
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://pipesheet.vercel.app/">
+        <meta property="og:title" content="PipeSheet">
+        <meta property="og:description" content="Integre seus pipes e planilhas">
+        <meta property="og:image" content="">
+
+        <!-- Twitter -->
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:url" content="https://pipesheet.vercel.app/">
+        <meta property="twitter:title" content="PipeSheet">
+        <meta property="twitter:description" content="Integre seus pipes e planilhas">
+        <meta property="twitter:image" content="">
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
