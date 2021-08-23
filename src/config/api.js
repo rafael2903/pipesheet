e45493195866b7c28d5e8572c0d4e27f87f4a92f
Pedro Henrique dos Santos <pedro.santos@cjr.org.br>
2021-08-23 10:00:57 -0300

    🧪 Testing

--- src/config/api.js
+++ src/config/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL:
     process.env.NODE_ENV == 'development'
       ? 'https://localhost:3000/api'
-      : process.env.NEXT_PUBLIC_VERCEL_URL + '/api',
+      : process.env.NEXT_PUBLIC_VERCEL_URL,
   headers: {
     'Content-Type': 'application/json',
   },
