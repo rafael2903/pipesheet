98e18e8d509fc3e9d5432bc2caa22a9bc91ef4e7
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2021-08-23 15:27:56 -0300

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL:
     process.env.NODE_ENV == 'development'
       ? 'https://localhost:3000/api'
-      : `/api`,
+      : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
