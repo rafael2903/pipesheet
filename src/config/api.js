ba5c304407d0f5549c37da9daa06d630cf517aa7
Pedro Henrique dos Santos <pedro.santos@cjr.org.br>
2021-08-23 09:11:30 -0300

    🐛 Fixes wrong api route

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: (process.env.VERCEL_URL || 'http://localhost:3000') + '/api',
   headers: {
     'Content-Type': 'application/json',
   },
