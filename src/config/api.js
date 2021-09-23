1b6bd2961e4a6c1d620aaef7359f16492934ef1c
adminCJR <admin@cjr.org.br>
2021-09-23 10:58:51 -0300

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL:
     process.env.NODE_ENV == 'development'
       ? 'http://localhost:3000/api'
-      : 'https://pipesheet.netlify.app/api',
+      : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
