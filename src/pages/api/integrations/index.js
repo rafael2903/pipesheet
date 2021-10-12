e8bf69386c104ce757a2ba55879e870f1a1b879e
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2021-10-12 14:26:15 -0300

    ✨ feat: create integrations CRUD

--- src/pages/api/integrations/index.js
+++ src/pages/api/integrations/index.js
@@ -1,31 +1,17 @@
 import nc from 'next-connect'
-import Integrations from 'models/integrations'
-import ddbClient from 'config/ddbClient'
-import { nanoid } from 'nanoid'
-import { PutItemCommand } from '@aws-sdk/client-dynamodb'
+import Integrations from 'controllers/integrations'
 
 const handler = nc()
   .post(async (req, res) => {
     const { pipeId, spreadsheetId, sheetId, title } = req.body
+
     try {
-
-      const input = {
-        id: { "S": nanoid() },
-        title: { "S": title },
-        pipeId: { "N": pipeId },
-        spreadsheetId: { "S": spreadsheetId },
-        sheetId: { "N": sheetId.toString() }
-      }
-
-      const params = {
-        TableName: "Integrations",
-        Item: input
-      }
-
-      const command = new PutItemCommand(params)
-
-      const response = await ddbClient.send(command)
-
+      const response = Integrations.create({
+        pipeId,
+        spreadsheetId,
+        sheetId,
+        title,
+      })
       res.status(200).json({ response })
     } catch (error) {
       res.status(500).json({ error: error.message })
